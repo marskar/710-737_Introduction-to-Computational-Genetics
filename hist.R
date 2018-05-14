689296abf8938302907bc8acbaf3d8c3dd821b87
Martin Skarzynski <marskar@gmail.com>
2018-05-14 14:23:49 -0400

    finished the hw

---
+++ hist.R
@@ -0,0 +1,8 @@
+#read in data
+starts <- read.table("WJHtrim30summary.tsv", stringsAsFactors=FALSE, header = FALSE) 
+
+#remove zeros
+starts <- starts[starts$V2!=0,]
+
+#make histogram
+hist(starts$V2, xlab = "Start Positions", main = "Frequency of Start Positions on Chromosome 19")
